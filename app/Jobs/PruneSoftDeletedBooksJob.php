041f1a1a0b520a931696e2060dae46ead895d578
YousefShamrokh <yousefshamrokh@gmail.com>
2026-07-14 14:02:12 +0300

    Job Scheduling Tasks

---
+++ app/Jobs/PruneSoftDeletedBooksJob.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Models\Book;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+
+class PruneSoftDeletedBooksJob implements ShouldQueue
+{
+    use Queueable, Dispatchable, InteractsWithQueue, SerializesModels;
+
+    /**
+     * Create a new job instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Execute the job.
+     */
+    public function handle(): void
+    {
+        Book::withTrashed()->where('deleted_at', '<=', now()->days(7))->forceDelete();
+    }
+}
--- routes/console.php
+++ routes/console.php
@@ -2,7 +2,13 @@
 
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use App\Jobs\PruneSoftDeletedBooksJob;
+use Illuminate\Support\Facades\Schedule;
 
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote');
+
+Schedule::job(new PruneSoftDeletedBooksJob())->daily()->at('00:00')->timezone('Africa/Cairo');
+
+// PruneSoftDeletedBooksJob::dispatch(); used to test if it works, but now it is scheduled to run daily at midnight
