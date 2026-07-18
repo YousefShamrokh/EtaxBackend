38ec2aeb86b35d6d5b0b9fb2a090f96a440dff30
YousefShamrokh <yousefshamrokh@gmail.com>
2026-07-18 12:36:46 +0300

    Modified attachment resource to only return respective ids, og file name, and url

--- app/Http/Resources/AttachmentResource.php
+++ app/Http/Resources/AttachmentResource.php
@@ -19,12 +19,7 @@ class AttachmentResource extends JsonResource
             'id' => $this->id,
             'book_id' => $this->book_id,
             'original_file_name' => $this->original_file_name,
-            'stored_file_name' => $this->stored_file_name,
-            'file_path' => $this->file_path,
-            'mime_type' => $this->mime_type,
             'url' => Storage::url($this->file_path),
-            'created_at' => $this->created_at,
-            'updated_at' => $this->updated_at,
         ];
     }
 }
